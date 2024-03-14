439d5208ded27a7de5e93a76249a144735f058fb
Andrew Lobban <andrewglobban@gmail.com>
2024-03-14 14:32:07 -0500

    Access to fields on Keyboard display

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -35,6 +35,7 @@ class _ExpensesState extends State<Expenses> {
       builder: (ctx) => NewExpense(
         onAddExpense: _addExpense,
       ),
+      constraints: const BoxConstraints(maxWidth: double.infinity),
     );
   }
 
